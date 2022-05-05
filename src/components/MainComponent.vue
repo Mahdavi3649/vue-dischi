8c17a0a35df7fce6e8bb35ff77d91709b0915b29
Mahdavi3649 <ivadham.smay3649@gmail.com>
2022-05-05 16:45:00 +0200

    added MainComponent

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,9 +1,14 @@
 <template>
   <main>
     <div class="container">
-      <div class="row">
-        <div class="col-2" v-for="(album, index) in musics" :key="index">
+      <div class="row p-5">
+        <div class="col-2-5" v-for="(album, index) in musics" :key="index">
           <img :src="album.poster" />
+          <h3 class="fs-6 text-center text-white fw-bold mt-3 text-uppercase">
+            {{ album.title }}
+          </h3>
+          <div class="caption mt-3">{{ album.author }}</div>
+          <div class="caption">{{ album.year }}</div>
         </div>
       </div>
     </div>
@@ -45,11 +50,22 @@ export default {
 <style scoped lang="scss">
 main {
   background-color: #1e2d3b;
-  height: 100vh;
+  img {
+    max-width: 100%;
+  }
+}
+.col-2-5 {
+  flex: 0 0 auto;
+  width: 18%;
+  margin: 1%;
+  padding: 25px;
+  background-color: #2e3a46;
 }
 
-.row {
-  display: flex;
-  flex-wrap: wrap;
+.caption {
+  text-align: center;
+  font-size: 14px;
+  color: #807f7f;
+  font-weight: 500;
 }
 </style>
