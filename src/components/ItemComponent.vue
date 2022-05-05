ba02ec2ae43b1e7488435d61d8a151b16d7fd3f4
Mahdavi3649 <ivadham.smay3649@gmail.com>
2022-05-05 17:19:54 +0200

    Add loader

---
+++ src/components/ItemComponent.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="col-2-5">
+    <img :src="album.poster" />
+    <h3 class="fs-6 text-center text-white fw-bold mt-3 text-uppercase">
+      {{ album.title }}
+    </h3>
+    <div class="caption mt-3">{{ album.author }}</div>
+    <div class="caption">{{ album.year }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ItemComponent",
+  props: {
+    album: Object,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+img {
+  max-width: 100%;
+}
+.col-2-5 {
+  flex: 0 0 auto;
+  width: 18%;
+  margin: 1%;
+  padding: 25px;
+  background-color: #2e3a46;
+}
+
+.caption {
+  text-align: center;
+  font-size: 14px;
+  color: #807f7f;
+  font-weight: 500;
+}
+</style>
--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,29 +1,40 @@
 <template>
   <main>
-    <div class="container">
-      <div class="row p-5">
-        <div class="col-2-5" v-for="(album, index) in musics" :key="index">
-          <img :src="album.poster" />
-          <h3 class="fs-6 text-center text-white fw-bold mt-3 text-uppercase">
-            {{ album.title }}
-          </h3>
-          <div class="caption mt-3">{{ album.author }}</div>
-          <div class="caption">{{ album.year }}</div>
+    <section v-if="!loading">
+      <div class="container">
+        <div class="row p-5">
+          <ItemComponent
+            :album="album"
+            v-for="(album, index) in musics"
+            :key="index"
+          />
         </div>
       </div>
+    </section>
+    <div
+      class="d-flex min-vh-100 align-items-center justify-content-center"
+      v-else
+    >
+      <h3>loading...</h3>
     </div>
   </main>
 </template>
 
 <script>
 import axios from "axios";
+import ItemComponent from "@/components/ItemComponent.vue";
+
 export default {
   name: "MainComponent",
+  components: {
+    ItemComponent,
+  },
 
   data() {
     return {
       link: "https://flynn.boolean.careers/exercises/api/array/music",
       musics: null,
+      loading: true,
     };
   },
 
@@ -39,6 +50,7 @@ export default {
       .then((response) => {
         console.log(response);
         this.musics = response.data.response;
+        this.loading = false;
       })
       .catch((error) => {
         console.log(error);
@@ -50,22 +62,5 @@ export default {
 <style scoped lang="scss">
 main {
   background-color: #1e2d3b;
-  img {
-    max-width: 100%;
-  }
-}
-.col-2-5 {
-  flex: 0 0 auto;
-  width: 18%;
-  margin: 1%;
-  padding: 25px;
-  background-color: #2e3a46;
-}
-
-.caption {
-  text-align: center;
-  font-size: 14px;
-  color: #807f7f;
-  font-weight: 500;
 }
 </style>
