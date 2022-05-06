0f5021281aace56653be8ce4ebbc8e5a5a92b44d
Mahdavi3649 <ivadham.smay3649@gmail.com>
2022-05-06 14:06:47 +0200

    Add input search

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,8 +1,23 @@
 <template>
-  <header>
-    <div class="logo">
+  <header class="d-flex justify-content-between align-items-center">
+    <div class="d-flex">
       <img src="@/assets/img/logo.png" alt="" />
     </div>
+    <form class="d-flex">
+      <input
+        class="form-control me-2"
+        type="search"
+        placeholder="Search"
+        aria-label="Search"
+      />
+      <button
+        class="btn btn-outline-success d-flex align-items-center"
+        type="submit"
+      >
+        <font-awesome-icon icon="fa-solid fa-magnifying-glass" />
+        Search
+      </button>
+    </form>
   </header>
 </template>
 
@@ -18,8 +33,12 @@ header {
 
   img {
     width: 45px;
-    margin: 0.6rem;
-    padding: 0.3rem 0;
+    margin: 0.8rem 1rem;
+  }
+  form {
+    height: 35px;
+    width: 20rem;
+    margin: 0.8rem 1rem;
   }
 }
 </style>
